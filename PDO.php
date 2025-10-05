6eea00f02210ebb1ef20d5b0f498092007d5f72a
binhb2111831 <binhhuynh190317@gmail.com>
2025-10-05 18:58:02 +0700

    Tao ket noi den csdl

---
+++ PDO.php
@@ -0,0 +1,18 @@
+<?php
+require 'vendor/autoload.php';
+
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+$dotenv->load();
+
+$servername = $_ENV['DB_HOST'];
+$dbname = $_ENV['DB_NAME'];
+$username = $_ENV['DB_USER'];
+$password = $_ENV['DB_PASS'];
+
+try {
+    $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    error_log("Connected successfully");
+} catch (PDOException $e) {
+    error_log("Connection failed: " . $e->getMessage());
+}
